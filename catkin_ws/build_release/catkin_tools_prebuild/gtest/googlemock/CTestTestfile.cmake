8fa0f1652a17eba9c72319179899d9443fa713b7
kai <zhaojiawe392@gmail.com>
2022-12-22 05:06:24 -0800

    first push

--- catkin_ws/build_release/catkin_tools_prebuild/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/kai/git/jzhao_suture2021/catkin_ws/build_release/catkin_tools_prebuild/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")

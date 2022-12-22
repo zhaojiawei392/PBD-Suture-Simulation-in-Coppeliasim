0b3cf0781a42a430e568b4e9ecfe19a24cb80469
kai <zhaojiawe392@gmail.com>
2022-12-22 00:34:50 -0800

    first push

---
+++ catkin_ws/build_release/jzhao_suture_simulation_ver31/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/kai/git/jzhao_suture2021/catkin_ws/build_release/jzhao_suture_simulation_ver31/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ catkin_ws/devel_release/share/jzhao_suture_simulation_ver31/cmake/jzhao_suture_simulation_ver31Config.cmake
@@ -0,0 +1,1 @@
+/home/kai/git/jzhao_suture2021/catkin_ws/devel_release/.private/jzhao_suture_simulation_ver31/share/jzhao_suture_simulation_ver31/cmake/jzhao_suture_simulation_ver31Config.cmake
